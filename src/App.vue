7cb04500d84757b2c0cac95be81646bd8d5f18b6
Chamel <chatmethar2@gmail.com>
2023-04-26 21:28:24 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container mx-auto my-12">
+    <div class="flex flex-col items-center">
+      <h2 class="text-4xl font-semibold text-blue-900 uppercase font-Kanit">Kankrai Line Login</h2>
+
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
